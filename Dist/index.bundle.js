72343aa43a23b73d2f3951bdd8fa35546d5613a5
kennethDRF <kennethramirezfernandez@gmail.com>
2023-08-21 21:28:17 -0600

    Tratando de borral la carpet Dist

--- Dist/index.bundle.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./Src/index.ts ***!
-  \**********************/
-console.log("Hola TypeScript");
-/******/ })()
-;
-//# sourceMappingURL=index.bundle.js.map
