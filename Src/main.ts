277a7a1f93147d4444530c8a0b2c382c768f7b3c
kennethDRF <kennethramirezfernandez@gmail.com>
2023-08-21 20:19:00 -0600

    Cambiando posicion de directorios

---
+++ Src/main.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC4UaPbhEsMrLlf9xQH-OiSyVLiE1nahbU",
+    authDomain: "todo-3e15a.firebaseapp.com",
+    projectId: "todo-3e15a",
+    storageBucket: "todo-3e15a.appspot.com",
+    messagingSenderId: "778207027427",
+    appId: "1:778207027427:web:dcc100dd74e65fcbc325d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ Src/typeScript.ts
@@ -0,0 +1,9 @@
+// Simepre debemos dejar que ts infiera los tipos de datos a medida de los posible 
+
+// Evitar los any y Function
+
+// Las funciones que " no tiene valor de retorno " devuelven void
+
+/* Tipos
+    Never, es usado con las funciones para indicar que nunca devolvera nada
+*/
